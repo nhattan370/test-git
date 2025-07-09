57e4a056d3b4939b942d8fdf8e69fcb5a5524b34
nha khoa <nhattanhmd123@gmail.com>
2025-07-09 07:08:14 +0700

    use hook useState to change value and show it after change

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useState} from 'react'
+//increase counter and show it on the screen 
 function App() {
+  const [counter, setCounter] = useState(1);
+  const handleCounter = () => {
+    setCounter(counter+1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{counter}</h1>
+      <button onClick={handleCounter}>Increase</button>
     </div>
   );
 }
